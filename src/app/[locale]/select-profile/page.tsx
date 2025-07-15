194fe5b4440ec21e901f1c18fe02002bba5946d9
ratchaphong <ratchaphongc1@gamil.com>
2025-07-15 11:15:03 +0700

    update file.

--- src/app/[locale]/select-profile/page.tsx
+++ src/app/[locale]/select-profile/page.tsx
@@ -8,29 +8,27 @@ const SelectProfile = () => {
   const { t, members, handleSelect } = useSelectProfile();
 
   return (
-    <main>
-      <div className={style.profile__wrapper}>
-        <h1 className={style.profile__title}>{t("title")}</h1>
-        <div className={style.profile__grid}>
-          {members.map((m) => (
-            <div key={m.name} className={style.profile__card}>
-              <div
-                className={style.profile__image}
-                onClick={() => handleSelect()}
-              >
-                <Image src={m.img} alt={m.name} width={128} height={128} />
-              </div>
-              <div className={style.profile__name}>
-                {m.name}
-                {m.locked && " 🔒"}
-              </div>
+    <main className={style.profile__wrapper}>
+      <h1 className={style.profile__title}>{t("title")}</h1>
+      <div className={style.profile__grid}>
+        {members.map((m) => (
+          <div key={m.name} className={style.profile__card}>
+            <div
+              className={style.profile__image}
+              onClick={() => handleSelect()}
+            >
+              <Image src={m.img} alt={m.name} width={128} height={128} />
             </div>
-          ))}
-        </div>
-        <button className={style.profile__manage_button}>
-          {t("manageProfiles")}
-        </button>
+            <div className={style.profile__name}>
+              {m.name}
+              {m.locked && " 🔒"}
+            </div>
+          </div>
+        ))}
       </div>
+      <button className={style.profile__manage_button}>
+        {t("manageProfiles")}
+      </button>
     </main>
   );
 };
