9d323c5a776474348c5e884aa3e5a5904e091cec
ratchaphong <ratchaphongc1@gamil.com>
2025-07-20 11:38:08 +0700

    add dropdown for change language

--- src/components/DropdownMenu/DropdownMenu.hooks.ts
+++ src/components/DropdownMenu/DropdownMenu.hooks.ts
@@ -1,6 +1,17 @@
+import { usePathname, useRouter } from "@/i18n/navigation";
 import { useAuthStore } from "@/stores/auth.store";
+import { useLocale } from "next-intl";
 
 export default function useDropdownMenu() {
   const { logout } = useAuthStore();
-  return { logout };
+  const router = useRouter();
+  const pathname = usePathname();
+  const locale = useLocale();
+
+  const handleChangeLocale = (targetLocale: string) => {
+    if (targetLocale === locale) return;
+    router.replace(pathname, { locale: targetLocale });
+  };
+
+  return { logout, handleChangeLocale, locale };
 }
--- src/components/DropdownMenu/index.tsx
+++ src/components/DropdownMenu/index.tsx
@@ -4,7 +4,7 @@ import { DropdownMenuProps } from "./DropdownMenu.types";
 import useDropdownMenu from "./DropdownMenu.hooks";
 
 export default function DropdownMenu({ profile, t }: DropdownMenuProps) {
-  const { logout } = useDropdownMenu();
+  const { logout, handleChangeLocale, locale } = useDropdownMenu();
   const router = useRouter();
 
   return (
@@ -19,6 +19,14 @@ export default function DropdownMenu({ profile, t }: DropdownMenuProps) {
           <li onClick={() => router.push("/select-profile")}>
             {t("manageProfile")}
           </li>
+          {["th", "en"]
+            .filter((lang) => lang !== locale)
+            .map((lang) => (
+              <li key={lang} onClick={() => handleChangeLocale(lang)}>
+                {/* {lang === "th" ? "🇹🇭 ภาษาไทย" : "🇺🇸 English"} */}
+                {t("changeLanguage")}
+              </li>
+            ))}
           <li onClick={logout}>{t("signOut")}</li>
         </ul>
       </div>
